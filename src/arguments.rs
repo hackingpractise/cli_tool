4cf0a85799a4ccd882ed429a318d708cb88a21a4
hackingpractise <sizeabe1024@skiff.com>
2024-12-13 20:34:35 +0900

    Add help to each of the arguments

--- src/arguments.rs
+++ src/arguments.rs
@@ -6,10 +6,10 @@ use std::path::PathBuf;
 #[command(name = "cli tool")]
 #[command(version, about)]
 pub struct Cli {
-    #[arg(long, short, num_args = 1.., value_parser = value_parser!(PathBuf), required = true)]
+    #[arg(long, short, num_args = 1.., value_parser = value_parser!(PathBuf), required = true, help = "Files that will be linked.")]
     pub source: Vec<PathBuf>,
-    #[arg(long, short, value_parser = value_parser!(PathBuf), required = true)]
+    #[arg(long, short, value_parser = value_parser!(PathBuf), required = true, help = "Directory or File where it will be placed")]
     pub target: PathBuf,
-    #[arg(long, short)]
+    #[arg(long, short, help = "If option is provided the symlink is removed.")]
     pub remove: bool,
 }
